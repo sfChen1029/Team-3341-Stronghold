cf8494dd40addd2a41c67fdb44e6718960414ac1
dia235 <vsengupta1@gmail.com>
2016-01-30 15:40:37 -0800

    Made the Acquirer Subsystem

---
+++ src/Subsystems/Acquirer.cpp
@@ -0,0 +1,17 @@
+#include <Subsystems/Acquirer.h>
+#include "../RobotMap.h"
+
+Acquirer::Acquirer() :
+		Subsystem("ExampleSubsystem")
+{
+
+}
+
+void Acquirer::InitDefaultCommand()
+{
+	// Set the default command for a subsystem here.
+	//SetDefaultCommand(new MySpecialCommand());
+}
+
+// Put methods for controlling this subsystem
+// here. Call these from Commands.
