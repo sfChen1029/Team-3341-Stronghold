6b3c968ff29d654f422a5e080b647ce76003fe74
abhinavkadiyala <abhinav_kadiyala@yahoo.co>
2016-01-30 16:36:00 -0800

    vas second

---
+++ src/Subsystems/Arm.h
@@ -0,0 +1,17 @@
+#ifndef Arm_H
+#define Arm_H
+
+#include "Commands/Subsystem.h"
+#include "WPILib.h"
+
+class Arm: public Subsystem
+{
+private:
+	// It's desirable that everything possible under private except
+	// for methods that implement subsystem capabilities
+public:
+	Arm();
+	void InitDefaultCommand();
+};
+
+#endif
