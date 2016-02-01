b355f014eec39f41dcf883d335dbff48c27d96dc
abhinavkadiyala <abhinav_kadiyala@yahoo.co>
2016-01-31 18:19:35 -0800

    added some functions to Arm subsystem

--- src/Subsystems/Arm.cpp
+++ src/Subsystems/Arm.cpp
@@ -2,15 +2,27 @@
 #include "../RobotMap.h"
 
 Arm::Arm() :
-		Subsystem("ExampleSubsystem")
+		Subsystem("Arm")
 {
-
+	encoder = new Encoder(ENCODER_LEFT_1,ENCODER_LEFT_2);
+	motor = new Jaguar(DRIVE_LEFT);
+	speed = 0;
 }
 
 void Arm::InitDefaultCommand()
 {
 	// Set the default command for a subsystem here.
 	//SetDefaultCommand(new MySpecialCommand());
+}
+
+void Arm::MoveArm(float speed)
+{
+	motor->Set(speed);
+}
+
+void Arm::StopArm()
+{
+	motor->Set(0);
 }
 
 // Put methods for controlling this subsystem
